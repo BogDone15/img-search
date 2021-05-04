30994c5b9ebf96176eb52a64c70205a313c0d2fa
BogDone <buselb2@gmail.com>
2021-05-04 15:29:12 +0300

    final adaptive

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ const API_KEY = '21389146-cca5561c436f6f4cadcc8ecb1';
 
 class App extends React.Component {
   state = {
-    images: []
+    images: [],
+    error: null
   }
 
   handleGetRequest = async e => {
@@ -16,18 +17,27 @@ class App extends React.Component {
     const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchTerm}&image_type=photo&pretty=true`;
     const request = await fetch(url);
     const response = await request.json();
-    this.setState({
-      images: response.hits,
-    })
-    console.log(searchTerm);
-    console.log(this.state.images);
+    if (!searchTerm) {
+      this.setState({
+        error: "Please provide a value."
+      })
+    } else {
+      this.setState({
+        images: response.hits,
+        error: null
+      })
+    }
   }
 
   render() {
     return (
       <div>
         <ImageSearch handleGetRequest={this.handleGetRequest} />
-        <ImageList images={this.state.images} />
+        {
+          this.state.error !== null ? 
+          <div style={{color: "#fff", textAlign: "center"}}>{ this.state.error }</div> :
+          <ImageList images={this.state.images} />
+        }
       </div>
     )
   }
--- src/ImageView/ImageView.js
+++ src/ImageView/ImageView.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import './ImageView.css';
 
-const ImageView = () => {
+import { Link } from 'react-router-dom';
+
+const ImageView = (props) => {
+ console.log(props);
+ const { largeImageURL: image, tags, user: owner, pageURL } = props.location.state.image;
  return (
-  <div>ImageView</div>
+  <div className="container">
+   <div className="row">
+    <div className="col-sm-12">
+     <div className="imageView__container">
+     <img src={image} alt={tags} className="imageView__img img-responsive" />
+     <p>&copy;pixabay</p>
+     <div className="imageView__text">
+     <h4>Credit: <span>{owner}</span></h4>
+     <h4>Download: <span><a target="_blank" href={pageURL}>GO TO DOWNLOAD</a></span></h4>
+     <button className="active-recipe__button"><Link to='/'>Home</Link></button>
+     </div>
+    </div>
+    </div>
+   </div>
+  </div>
  )
 }
 
